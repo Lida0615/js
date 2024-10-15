5da92b81941f0ca2b96235685602027f79087f7f
Lida <garbakovalida@mail.ru>
2024-10-15 20:47:31 +0300

    создавли 2 папку

---
+++ less01/app.js
@@ -0,0 +1,12 @@
+
+// let
+// const
+
+let message = 'привет из игс'
+message = 123123
+
+const num = 2024
+num = 123
+console.log(num )
+
+
---
+++ less02/app.js
@@ -0,0 +1,12 @@
+let num = 10
+
+console.log(num)
+
+// num = num + 1
+console.log(num++)
+
+console.log(num)
+// console.log(num++)
+
+// num = num + 1
+// console.log(num)
